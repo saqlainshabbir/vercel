130ffe237d3abd35d6ad387fe4441825d75314d4
Muhammad Saqlain <154231070+saqlainshabbir@users.noreply.github.com>
2024-10-01 17:43:54 +0500

    5

--- main.py
+++ main.py
@@ -2,16 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, welcome to the deployed project on Vercel!"
-
-@app.route('/math')
-def math():
-    num1 = 5
-    num2 = 10
-    result = num1 + num2
-    return f"The result of adding {num1} and {num2} is {result}"
+print("Hello, welcome to the deployed project on Vercel!")
 
 if __name__ == "__main__":
     app.run()
