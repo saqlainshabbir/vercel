8b203f4e2e2a4ac83571a6121dcbb3327b98726a
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-10-01 15:00:55 +0500

    2

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-def print_greetings():
-    print("Hello, welcome to my project!")
-    print("Hope you have a great day!")
-
-def print_math_results():
-    num1 = 5
-    num2 = 10
-    result = num1 + num2
-    print(f"The result of adding {num1} and {num2} is {result}")
-
-def main():
-    print_greetings()
-    print("\n---\n")
-    print_math_results()
-
-if __name__ == "__main__":
-    main()
+print("Hello Vercel!")
