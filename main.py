49b3934f19333cd032fd7c3ef892c1d6fbe15bd6
Muhammad Saqlain <154231070+saqlainshabbir@users.noreply.github.com>
2024-10-01 16:06:44 +0500

    1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello Vercel!")
-print("Hello Vercel!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, welcome to the deployed project on Vercel!"
+
+@app.route('/math')
+def math():
+    num1 = 5
+    num2 = 10
+    result = num1 + num2
+    return f"The result of adding {num1} and {num2} is {result}"
+
+if __name__ == "__main__":
+    app.run()
